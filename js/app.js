5274ec05cf54f993ecc17fc3b43655b552856a30
Vanya <vfunez@ucvts.org>
2020-01-08 19:25:02 -0500

    sevententh commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const clear = document.querySelector(".clear");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-plus-square-o";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+const NORMAL = "fa-exclamation";
+const PRIORITY = "fa-exclamation-circle";
+
+let LIST = []
+    , id = 0;
